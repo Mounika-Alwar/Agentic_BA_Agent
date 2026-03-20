828b4c3bfb2f9ef9c267107421afc3a0af23db2e
Mounika-Alwar <mounikaalwar@outlook.com>
2026-03-20 19:23:50 +0530

    Update import statement for PromptTemplate

--- agents/planner_agent.py
+++ agents/planner_agent.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 import os
 
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 
 
 
